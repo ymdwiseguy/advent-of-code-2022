18f7cebf5ff772c0d291c034dc3f7139b4bf090a
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2022-12-06 08:46:46 +0100

    solve day 1

--- src/main/kotlin/day1/Day01.kt
+++ src/main/kotlin/day1/Day01.kt
@@ -1,18 +1,56 @@
 package day1
 
 import readInput
+import kotlin.Int as Calories
+
+fun main() {
+    println("Part 1: ${Day01().part1(Day01().input)}")
+    println("Part 2: ${Day01().part2(Day01().input)}")
+}
 
 class Day01 {
 
     val input = readInput("resources/day01/data")
     val testInput = readInput("resources/day01/data_test")
 
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Calories = getAllElves(input).maxOf { it.calories() }
+
+
+    fun part2(input: List<String>): Calories {
+        val elves = getAllElves(input)
+
+        val highestCalories = elves.maxOf { it.calories() }
+
+        val restElves = elves.filter { it.calories() != highestCalories }
+
+        val secondHighestCalories = restElves.maxOf { it.calories() }
+
+        val restElves2 = restElves.filter { it.calories() != secondHighestCalories }
+
+        return highestCalories + secondHighestCalories + restElves2.maxOf { it.calories() }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    private fun getAllElves(input: List<String>): MutableList<Elf> {
+        val elves = mutableListOf<Elf>()
+        var currentElf = Elf(mutableListOf())
+
+        input.forEach {
+            if (it != "") {
+                currentElf.food.add(it.toInt())
+            } else {
+                elves.add(currentElf)
+                currentElf = Elf(mutableListOf())
+            }
+        }
+        elves.add(currentElf)
+        return elves
+    }
+
+    data class Elf(
+        val food: MutableList<Calories>
+    ) {
+        fun calories(): Calories = food.sum()
+
     }
 
 }
--- src/test/kotlin/day1/Day01KtTest.kt
+++ src/test/kotlin/day1/Day01KtTest.kt
@@ -4,23 +4,31 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import shouldBe
 
-class Day01Test{
+class Day01Test {
 
     private lateinit var day: Day01
     private val input: List<String> = Day01().testInput
 
     @BeforeEach
-    fun before(){
+    fun before() {
         day = Day01()
     }
 
     @Test
-    fun `part 1 should return number of increases`() {
-        day.part1(input) shouldBe 0
+    fun `calculate an elves calories`() {
+        val elf = Day01.Elf(
+            mutableListOf(1000, 2000, 3000)
+        )
+        elf.calories() shouldBe 6000
     }
 
     @Test
-    fun `part 2 should return number of sliding window sum increases`() {
-         day.part2(input) shouldBe 0
+    fun `part 1 should return highest calories value`() {
+        day.part1(input) shouldBe 24000
+    }
+
+    @Test
+    fun `part 2 should return top three calories summarized`() {
+        day.part2(input) shouldBe 45000
     }
 }
