2d3f4388bd96ebc1f2c38815f9896fda569db530
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2022-12-07 21:26:20 +0100

    solve day 2

--- src/main/kotlin/day2/Day02.kt
+++ src/main/kotlin/day2/Day02.kt
@@ -2,18 +2,109 @@ package day2
 
 import readInput
 
+fun main() {
+    println("Part 1: ${Day02().part1(Day02().input)}")
+    println("Part 2: ${Day02().part2(Day02().input)}")
+}
+
 class Day02 {
 
     val input = readInput("resources/day02/data")
     val testInput = readInput("resources/day02/data_test")
 
     fun part1(input: List<String>): Int {
-        return 0
+        var score = 0
+        input.map {
+            val (player1, player2) = it.split(" ")
+            score += findScoreForPlayer2(player1, player2)
+        }
+        return score
     }
 
-    fun part2(input: List<String>): Int  {
-        return 0
+    fun part2(input: List<String>): Int {
+        var score = 0
+        input.map {
+            val (player1, necessaryResult) = it.split(" ")
+            score += findAnswerForPlayer2(player1, necessaryResult)
+        }
+        return score
     }
+
+    // Rock A & X
+    // Paper B & Y
+    // Scissors C & Z
+    fun findScoreForPlayer2(player1: String, player2: String): Int = when (player2) {
+        "X" -> {
+            1 + when (player1) {
+                "A" -> 3
+                "B" -> 0
+                "C" -> 6
+                else -> 0
+            }
+        }
+
+        "Y" -> {
+            2 + when (player1) {
+                "A" -> 6
+                "B" -> 3
+                "C" -> 0
+                else -> 0
+            }
+        }
+
+        "Z" -> {
+            3 + when (player1) {
+                "A" -> 0
+                "B" -> 6
+                "C" -> 3
+                else -> 0
+            }
+        }
+
+        else -> {
+            0
+        }
+    }
+
+    // Rock A -> 1
+    // Paper B -> 2
+    // Scissors C -> 3
+    // Loose X -> 0
+    // Draw Y -> 3
+    // Win Z -> 6
+    fun findAnswerForPlayer2(player: String, necessaryResult: String): Int = when (necessaryResult) {
+        "X" -> {
+            0 + when (player) {
+                "A" -> 3 // C
+                "B" -> 1 // A
+                "C" -> 2 // B
+                else -> 0
+            }
+        }
+
+        "Y" -> {
+            3 + when (player) {
+                "A" -> 1 // A
+                "B" -> 2 // B
+                "C" -> 3 // C
+                else -> 0
+            }
+        }
+
+        "Z" -> {
+            6 + when (player) {
+                "A" -> 2 // B
+                "B" -> 3 // C
+                "C" -> 1 // A
+                else -> 0
+            }
+        }
+
+        else -> {
+            0
+        }
+    }
+
 
 }
 
--- src/test/kotlin/day2/Day02KtTest.kt
+++ src/test/kotlin/day2/Day02KtTest.kt
@@ -4,23 +4,37 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import shouldBe
 
-class Day02Test{
+class Day02Test {
 
     private lateinit var day: Day02
     private val input: List<String> = Day02().testInput
 
     @BeforeEach
-    fun before(){
+    fun before() {
         day = Day02()
     }
 
     @Test
-    fun `part 1 should return navigation result`() {
-        day.part1(input) shouldBe 0
+    fun `find score`() {
+        day.findScoreForPlayer2("A", "Y") shouldBe 8
+        day.findScoreForPlayer2("B", "X") shouldBe 1
+        day.findScoreForPlayer2("C", "Z") shouldBe 6
     }
 
     @Test
-    fun `part 2 should return navigation result`() {
-        day.part2(input) shouldBe 0
+    fun `find needed result`() {
+        day.findAnswerForPlayer2("A", "Y") shouldBe 4
+        day.findAnswerForPlayer2("B", "X") shouldBe 1
+        day.findAnswerForPlayer2("C", "Z") shouldBe 7
+    }
+
+    @Test
+    fun `part 1 should score 15 points`() {
+        day.part1(input) shouldBe 15
+    }
+
+    @Test
+    fun `part 2 should return 12`() {
+        day.part2(input) shouldBe 12
     }
 }
