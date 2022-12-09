d2547ace1013049b25e916c7f67dfc6fbced22e7
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2022-12-09 15:58:01 +0100

    solve day 4

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -12,7 +12,43 @@ class Day04 {
     val input = readInput("resources/day04/data")
     val testInput = readInput("resources/day04/data_test")
 
-    fun part1(input: List<String>): Int = 0
-    fun part2(input: List<String>): Int = 0
+    fun part1(input: List<String>): Int {
+        var count = 0
+        input.forEach {
+            if(compartmentsFullyOverlap(it)){
+                count++
+            }
+        }
+        return count
+    }
+
+    fun part2(input: List<String>): Int {
+        var count = 0
+        input.forEach {
+            if(compartmentsOverlap(it)){
+                count++
+            }
+        }
+        return count
+    }
+
+    private fun compartmentsOverlap(input: String): Boolean {
+        val (range1, range2) = getRanges(input)
+        return (range1.find { it in range2 } ?: 0) > 0 || (range2.find { it in range1 } ?: 0) > 0
+    }
+
+    fun compartmentsFullyOverlap(input: String): Boolean {
+        val (range1, range2) = getRanges(input)
+        return range1.all { it in range2 } || range2.all { it in range1 }
+    }
+
+    private fun getRanges(input: String): Pair<IntRange, IntRange> {
+        val (c1, c2) = input.split(",")
+        val (c1a, c1b) = c1.split("-")
+        val (c2a, c2b) = c2.split("-")
+        val range1 = c1a.toInt()..c1b.toInt()
+        val range2 = c2a.toInt()..c2b.toInt()
+        return Pair(range1, range2)
+    }
 
 }
--- src/test/kotlin/day04/Day04Test.kt
+++ src/test/kotlin/day04/Day04Test.kt
@@ -8,6 +8,7 @@ internal class Day04Test {
 
     private lateinit var day: Day04
     private val testInput: List<String> = Day04().testInput
+    private val input: List<String> = Day04().input
 
     @BeforeEach
     fun setUp() {
@@ -15,14 +16,33 @@ internal class Day04Test {
     }
 
     @Test
-    fun `part 1 should return 0`() {
-        day.part1(testInput) shouldBe 0
-
+    fun `part 1 should return 2`() {
+        day.part1(testInput) shouldBe 2
     }
 
     @Test
-    fun `part 2 should return 0`() {
-        day.part2(testInput) shouldBe 0
+    fun `part 1 with real data should return 602`() {
+        day.part1(input) shouldBe 602
+    }
+
+    @Test
+    fun `compartments fully overlap`() {
+         day.compartmentsFullyOverlap("2-4,6-8") shouldBe false
+         day.compartmentsFullyOverlap("2-3,4-5") shouldBe false
+         day.compartmentsFullyOverlap("5-7,7-9") shouldBe false
+         day.compartmentsFullyOverlap("2-8,3-7") shouldBe true
+         day.compartmentsFullyOverlap("6-6,4-6") shouldBe true
+         day.compartmentsFullyOverlap("2-6,4-8") shouldBe false
+    }
+
+    @Test
+    fun `part 2 should return 4`() {
+        day.part2(testInput) shouldBe 4
+    }
+
+    @Test
+    fun `part 2 should return 891`() {
+        day.part2(input) shouldBe 891
     }
 
 }
