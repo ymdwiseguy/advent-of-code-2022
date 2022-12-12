7987ab265fe501b193684571d42300ce22124ab0
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2022-12-12 12:17:47 +0100

    solve day 5 part 1

--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -12,7 +12,74 @@ class Day05 {
     val input = readInput("resources/day05/data")
     val testInput = readInput("resources/day05/data_test")
 
-    fun part1(input: List<String>): Int = 0
+    fun part1(input: List<String>): String {
+        val (stacks: MutableMap<Int, Stack>, instructions: List<Instruction>) = parseInput(input)
+
+        return stacks.followInstructions(instructions).map { mapEntry ->
+            mapEntry.value.crates.last()
+        }.joinToString(separator = "")
+    }
+
     fun part2(input: List<String>): Int = 0
 
+
+    fun parseInput(input: List<String>): Pair<MutableMap<Int, Stack>, List<Instruction>> =
+        input.indexOf("").let { emptyRowIndex ->
+            Pair(
+                parseStacks(input.subList(0, emptyRowIndex)),
+                parseInstructions(input.subList(emptyRowIndex + 1, input.size))
+            )
+        }
+
+    fun parseInstructions(strings: List<String>): List<Instruction> = strings.map {
+        Instruction(
+            howMany = it.split("move ").last().split(" from").first().toInt(),
+            from = it.split("from ").last().split(" to").first().toInt(),
+            to = it.split("to ").last().toInt()
+        )
+    }
+
+    fun parseStacks(strings: List<String>): MutableMap<Int, Stack> {
+        val stacks = mutableMapOf<Int, Stack>()
+        for (i in 1..strings.last().windowed(2, 4).size) {
+            stacks[i] = Stack(mutableListOf())
+        }
+
+        strings.reversed().forEachIndexed { rowIndex, row ->
+            if (rowIndex > 0) {
+                row.windowed(2, 4).forEachIndexed { stackId, chars ->
+                    val c = chars.last()
+                    if (!c.isWhitespace()) {
+                        stacks[stackId + 1]?.crates?.add(c)
+                    }
+                }
+            }
+        }
+        return stacks
+    }
+
+    data class Stack(
+        val crates: MutableList<Char> = mutableListOf()
+    )
+
+    data class Instruction(
+        val howMany: Int,
+        val from: Int,
+        val to: Int,
+    )
 }
+
+fun MutableMap<Int, Day05.Stack>.followInstructions(instructions: List<Day05.Instruction>): MutableMap<Int, Day05.Stack> {
+    instructions.forEach {
+        for (i in 1..it.howMany) {
+            this[it.from]?.crates?.let { cratesFromStack ->
+                this[it.to]?.crates?.add(
+                    cratesFromStack.last()
+                )
+                cratesFromStack.removeLast()
+            }
+        }
+    }
+    return this
+}
+
--- src/test/kotlin/day05/Day05Test.kt
+++ src/test/kotlin/day05/Day05Test.kt
@@ -1,5 +1,7 @@
 package day05
 
+import day05.Day05.Instruction
+import day05.Day05.Stack
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import shouldBe
@@ -8,6 +10,7 @@ internal class Day05Test {
 
     private lateinit var day: Day05
     private val testInput: List<String> = Day05().testInput
+    private val input: List<String> = Day05().input
 
     @BeforeEach
     fun setUp() {
@@ -15,9 +18,76 @@ internal class Day05Test {
     }
 
     @Test
-    fun `part 1 should return 0`() {
-        day.part1(testInput) shouldBe 0
+    fun `part 1 should return CMZ`() {
+        day.part1(testInput) shouldBe "CMZ"
+    }
 
+    @Test
+    fun `part 1 with real data should return NTWZZWHFV`() {
+         day.part1(input) shouldBe "NTWZZWHFV"
+    }
+
+    @Test
+    fun `compute instructions`() {
+        val (stacks: Map<Int, Stack>, instructions: List<Instruction>) = day.parseInput(
+            testInput
+        )
+
+        stacks.followInstructions(instructions) shouldBe mapOf(
+            1 to Stack(mutableListOf('C')),
+            2 to Stack(mutableListOf('M')),
+            3 to Stack(mutableListOf('P', 'D', 'N', 'Z')),
+        )
+    }
+
+    @Test
+    fun `parse instructions`() {
+        day.parseInstructions(
+            listOf(
+                "move 1 from 2 to 1",
+                "move 3 from 1 to 3",
+                "move 2 from 2 to 1",
+                "move 1 from 1 to 2"
+            )
+        ) shouldBe listOf(
+            Instruction(1, 2, 1),
+            Instruction(3, 1, 3),
+            Instruction(2, 2, 1),
+            Instruction(1, 1, 2),
+        )
+    }
+
+    @Test
+    fun `number of stacks`() {
+        stringsOfStacks.last().windowed(2, 4).size shouldBe 3
+    }
+
+    @Test
+    fun `parse stacks`() {
+        day.parseStacks(
+            stringsOfStacks
+        ) shouldBe mapOf(
+            1 to Stack(mutableListOf('Z', 'N')),
+            2 to Stack(mutableListOf('M', 'C', 'D')),
+            3 to Stack(mutableListOf('P')),
+        )
+    }
+
+    @Test
+    fun `parse input`() {
+        val result = day.parseInput(testInput)
+
+        result.first shouldBe mapOf(
+            1 to Stack(mutableListOf('Z', 'N')),
+            2 to Stack(mutableListOf('M', 'C', 'D')),
+            3 to Stack(mutableListOf('P')),
+        )
+        result.second shouldBe listOf(
+            Instruction(1, 2, 1),
+            Instruction(3, 1, 3),
+            Instruction(2, 2, 1),
+            Instruction(1, 1, 2),
+        )
     }
 
     @Test
@@ -25,4 +95,12 @@ internal class Day05Test {
         day.part2(testInput) shouldBe 0
     }
 
+    private companion object {
+        val stringsOfStacks = listOf(
+            "    [D]\n",
+            "[N] [C]\n",
+            "[Z] [M] [P]\n",
+            " 1   2   3"
+        )
+    }
 }
