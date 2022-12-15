fa5ed76e7072e130d09973586c52a0c7c6578e4d
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2022-12-15 20:36:55 +0100

    solve day 7

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,5 +1,7 @@
 package day07
 
+import day07.Day07.*
+import day07.Day07.Command.*
 import readInput
 
 fun main() {
@@ -12,7 +14,150 @@ class Day07 {
     val input = readInput("resources/day07/data")
     val testInput = readInput("resources/day07/data_test")
 
-    fun part1(input: List<String>): Int = 0
-    fun part2(input: List<String>): Int = 0
+    fun part1(input: List<String>): Int {
+        val (directoryTree, directories) = getDirectories(input)
 
+        val directorySizes = directories.sumOf {
+            val dirSize = directoryTree.gotoDir(it)?.directorySize() ?: 0
+            return@sumOf if (dirSize <= 100000) dirSize else 0
+        }
+
+        return directorySizes
+    }
+
+    fun part2(input: List<String>): Int {
+        val (directoryTree, directories) = getDirectories(input)
+        val spaceToFree = 30000000 - (70000000 - directoryTree.directorySize())
+
+        var perfectDirSize = Int.MAX_VALUE
+
+        directories.forEach {
+            val dirSize = directoryTree.gotoDir(it)?.directorySize() ?: 0
+            if (dirSize in spaceToFree..perfectDirSize) {
+                perfectDirSize = dirSize
+            }
+        }
+
+        return perfectDirSize
+    }
+
+
+    private fun getDirectories(input: List<String>): Pair<MutableMap<String, Directory>, MutableList<List<String>>> {
+        val directoryTree = mutableMapOf("/" to Directory())
+        val directories = mutableListOf(listOf("/"))
+        val currentPath = mutableListOf<String>()
+        val commands = getCommands(input)
+
+        commands.forEach {
+            directoryTree.parseCommand(it, currentPath, directories)
+        }
+        return Pair(directoryTree, directories)
+    }
+
+
+    fun getCommands(input: List<String>): List<Command> = input.map {
+        when {
+            it.startsWith("$ cd ..") -> NavigateUp
+            it.startsWith("$ cd") -> ChangeDirectory(
+                it.substring(5..it.lastIndex)
+            )
+
+            it.startsWith("$ ls") -> ListContent
+            it.startsWith("dir ") -> AddDirectory(
+                it.substring(4..it.lastIndex)
+            )
+
+            else -> {
+                val parts = it.split(" ")
+                return@map AddFile(parts.last(), parts.first().toInt())
+            }
+        }
+    }
+
+    sealed class Command {
+        data class ChangeDirectory(
+            val which: String
+        ) : Command()
+
+        object ListContent : Command()
+        data class AddDirectory(
+            val name: String
+        ) : Command()
+
+        data class AddFile(
+            val name: String,
+            val size: Int,
+        ) : Command()
+
+        object NavigateUp : Command()
+    }
+
+    data class Directory(
+        val subDirectories: MutableMap<String, Directory> = mutableMapOf(),
+        val files: MutableList<File> = mutableListOf(),
+    ) {
+        fun directorySize(): Int =
+            files.sumOf { it.size } + subDirectories.values.sumOf { directory -> directory.directorySize() }
+
+        override fun toString(): String {
+            return "${subDirectories.print()} " + files.map { it.toString() }
+        }
+    }
+
+    data class File(
+        val name: String,
+        val size: Int,
+    ) {
+        override fun toString(): String = "$name :: $size"
+    }
+}
+
+fun MutableMap<String, Directory>.print(): String = this.map {
+    "${it.key}: ${it.value}\n"
+}.joinToString("\n")
+
+fun MutableMap<String, Directory>.directorySize(): Int =
+    this.values.sumOf { it.directorySize() }
+
+fun MutableMap<String, Directory>.parseCommand(
+    command: Command,
+    currentPath: MutableList<String>,
+    directories: MutableList<List<String>>
+) {
+    when (command) {
+        is AddDirectory -> {
+            directories.add(currentPath.toList() + listOf(command.name))
+            gotoDir(currentPath.toList())?.subDirectories?.put(command.name, Directory())
+        }
+
+        is AddFile -> {
+            val file = File(command.name, command.size)
+            gotoDir(currentPath)?.files?.add(file)
+        }
+
+        is ChangeDirectory -> {
+            currentPath.add(command.which)
+        }
+
+        NavigateUp -> currentPath.removeLast()
+        else -> Unit
+    }
+}
+
+private fun MutableMap<String, Directory>.gotoDir(currentPath: List<String>): Directory? =
+    levelX(currentPath, currentPath.size)
+
+private fun MutableMap<String, Directory>.level1(currentPath: List<String>) =
+    this[currentPath[0]]
+
+private fun MutableMap<String, Directory>.level2(currentPath: List<String>) =
+    this[currentPath[0]]?.subDirectories?.get(currentPath[1])
+
+private fun MutableMap<String, Directory>.levelX(currentPath: List<String>, level: Int): Directory? {
+    return when {
+        level == 1 -> level1(currentPath)
+        level == 2 -> level2(currentPath)
+        level > 2 -> levelX(currentPath, (level - 1))?.subDirectories?.get(currentPath[level - 1])
+        else -> TODO()
+    }
 }
--- src/test/kotlin/day07/Day07Test.kt
+++ src/test/kotlin/day07/Day07Test.kt
@@ -1,5 +1,8 @@
 package day07
 
+import day07.Day07.Command.*
+import day07.Day07.Directory
+import day07.Day07.File
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import shouldBe
@@ -8,6 +11,7 @@ internal class Day07Test {
 
     private lateinit var day: Day07
     private val testInput: List<String> = Day07().testInput
+    private val input: List<String> = Day07().input
 
     @BeforeEach
     fun setUp() {
@@ -15,14 +19,212 @@ internal class Day07Test {
     }
 
     @Test
-    fun `part 1 should return 0`() {
-        day.part1(testInput) shouldBe 0
-
+    fun `part 1 should return 95437`() {
+        day.part1(testInput) shouldBe 95437
     }
 
     @Test
-    fun `part 2 should return 0`() {
-        day.part2(testInput) shouldBe 0
+    fun `part 1 with real data should be greater than 120994`() {
+        day.part1(input) shouldBe 1390824
     }
 
+    @Test
+    fun `part 2 should return 24933642`() {
+        day.part2(testInput) shouldBe 24933642
+    }
+
+    @Test
+    fun `part 2 with real data should return 7490863`() {
+        day.part2(input) shouldBe 7490863
+    }
+
+    @Test
+    fun `get commands from string`() {
+        day.getCommands(
+            listOf(
+                "$ cd /",
+                "$ ls",
+                "dir a",
+                "14848514 b.txt",
+                "8504156 c.dat",
+                "dir d",
+                "$ cd a",
+                "$ ls",
+                "dir e",
+                "29116 f",
+                "2557 g",
+                "62596 h.lst",
+                "$ cd e",
+                "$ ls",
+                "584 i",
+                "$ cd ..",
+                "$ cd ..",
+                "$ cd d",
+                "$ ls",
+                "4060174 j",
+                "8033020 d.log",
+                "5626152 d.ext",
+                "7214296 k",
+            )
+        ) shouldBe listOf(
+            ChangeDirectory("/"),
+            ListContent,
+            AddDirectory("a"),
+            AddFile("b.txt", 14848514),
+            AddFile("c.dat", 8504156),
+            AddDirectory("d"),
+            ChangeDirectory("a"),
+            ListContent,
+            AddDirectory("e"),
+            AddFile("f", 29116),
+            AddFile("g", 2557),
+            AddFile("h.lst", 62596),
+            ChangeDirectory("e"),
+            ListContent,
+            AddFile("i", 584),
+            NavigateUp,
+            NavigateUp,
+            ChangeDirectory("d"),
+            ListContent,
+            AddFile("j", 4060174),
+            AddFile("d.log", 8033020),
+            AddFile("d.ext", 5626152),
+            AddFile("k", 7214296),
+        )
+    }
+
+    @Test
+    fun `parse all commands`() {
+        val directoryTree = mutableMapOf("/" to Directory())
+        val directories = mutableListOf<List<String>>(listOf("/"))
+        val currentPath = mutableListOf<String>()
+
+        val commands = listOf(
+            ChangeDirectory("/"),
+            ListContent,
+            AddDirectory("a"),
+            AddFile("b.txt", 14848514),
+            AddFile("c.dat", 8504156),
+            AddDirectory("d"),
+            ChangeDirectory("a"),
+            ListContent,
+            AddDirectory("e"),
+            AddFile("f", 29116),
+            AddFile("g", 2557),
+            AddFile("h.lst", 62596),
+            ChangeDirectory("e"),
+            ListContent,
+            AddFile("i", 584),
+            NavigateUp,
+            NavigateUp,
+            ChangeDirectory("d"),
+            ListContent,
+            AddFile("j", 4060174),
+            AddFile("d.log", 8033020),
+            AddFile("d.ext", 5626152),
+            AddFile("k", 7214296),
+        )
+
+        commands.forEach {
+            directoryTree.parseCommand(it, currentPath, directories)
+        }
+
+        currentPath shouldBe mutableListOf("/", "d")
+        directoryTree shouldBe mutableMapOf(
+            "/" to Directory(
+                subDirectories = mutableMapOf(
+                    "a" to Directory(
+                        subDirectories = mutableMapOf(
+                            "e" to Directory(
+                                files = mutableListOf(
+                                    File("i", 584)
+                                )
+                            )
+                        ),
+                        files = mutableListOf(
+                            File("f", 29116),
+                            File("g", 2557),
+                            File("h.lst", 62596),
+                        )
+                    ),
+                    "d" to Directory(
+                        files = mutableListOf(
+                            File("j", 4060174),
+                            File("d.log", 8033020),
+                            File("d.ext", 5626152),
+                            File("k", 7214296),
+                        ),
+                    ),
+                ),
+                files = mutableListOf(
+                    File("b.txt", 14848514),
+                    File("c.dat", 8504156),
+                )
+            )
+        )
+
+        directories shouldBe mutableListOf(
+            listOf("/"),
+            listOf("/", "a"),
+            listOf("/", "d"),
+            listOf("/", "a", "e"),
+        )
+
+        directoryTree.directorySize() shouldBe 48381165
+    }
+
+    @Test
+    fun `get directory size`() {
+        Directory(
+            subDirectories = mutableMapOf(
+                "c" to Directory(
+                    files = mutableListOf(
+                        File("d", 20)
+                    )
+                )
+            ),
+            files = mutableListOf(
+                File("a", 5),
+                File("b", 5),
+
+                )
+        ).directorySize() shouldBe 30
+    }
+
+    @Test
+    fun `get directory size for whole tree`() {
+        mutableMapOf(
+            "/" to Directory(
+                subDirectories = mutableMapOf(
+                    "a" to Directory(
+                        subDirectories = mutableMapOf(
+                            "e" to Directory(
+                                files = mutableListOf(
+                                    File("i", 584)
+                                )
+                            )
+                        ),
+                        files = mutableListOf(
+                            File("f", 29116),
+                            File("g", 2557),
+                            File("h.lst", 62596),
+                        )
+                    ),
+                    "d" to Directory(
+                        files = mutableListOf(
+                            File("j", 4060174),
+                            File("d.log", 8033020),
+                            File("d.ext", 5626152),
+                            File("k", 7214296),
+                        ),
+                    ),
+                ),
+                files = mutableListOf(
+                    File("b.txt", 14848514),
+                    File("c.dat", 8504156),
+                )
+            )
+        ).directorySize() shouldBe 48381165
+    }
 }
+
