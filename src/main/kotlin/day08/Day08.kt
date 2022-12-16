bfa7a44a012d0e5ae2579ef188208aa2bec88ba0
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2022-12-16 12:24:43 +0100

    solve day 8

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -12,7 +12,117 @@ class Day08 {
     val input = readInput("resources/day08/data")
     val testInput = readInput("resources/day08/data_test")
 
-    fun part1(input: List<String>): Int = 0
-    fun part2(input: List<String>): Int = 0
+    fun part1(input: List<String>): Int {
+        var visibleCount = 0
+
+        getTreesFromInput(input).forEach {
+            if (it.isVisible()) visibleCount++
+        }
+
+        return visibleCount
+    }
+
+    fun part2(input: List<String>): Int {
+        var maxScenicScore = 0
+        var bestTree: Tree? = null
+        getTreesFromInput(input).forEach { tree ->
+            tree.scenicScore().let { score ->
+                if (score > maxScenicScore) {
+                    maxScenicScore = score
+                    bestTree = tree
+                }
+            }
+        }
+
+        println("Best Tree: ${bestTree?.print()}")
+        return maxScenicScore
+    }
+
+    private fun getTreesFromInput(input: List<String>): MutableList<Tree> {
+        val rows: List<List<Int>> = input.map { string -> string.map { it.digitToInt() } }
+        val columns = mutableMapOf<Int, MutableList<Int>>()
+
+        rows.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { columnIndex, treeHeight ->
+                if (rowIndex == 0) {
+                    columns[columnIndex] = mutableListOf(treeHeight)
+                } else {
+                    columns[columnIndex]?.add(treeHeight)
+                }
+            }
+        }
+
+        val trees = mutableListOf<Tree>()
+
+        rows.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { columnIndex, treeHeight ->
+                val column = columns[columnIndex]
+                val leftNeighbours = if (columnIndex == 0) emptyList() else row.slice(0 until columnIndex).reversed()
+                val rightNeighbours =
+                    if (columnIndex == row.lastIndex) emptyList() else row.slice(columnIndex + 1..row.lastIndex)
+                val topNeighbours = if (rowIndex == 0) emptyList() else {
+                    column?.slice(0 until rowIndex)?.reversed() ?: emptyList()
+                }
+                val bottomNeighbours =
+                    if (rowIndex == column?.lastIndex) emptyList() else column?.slice(rowIndex + 1..(column.lastIndex))
+                        ?: emptyList()
+                trees.add(
+                    Tree(
+                        position = Position(rowIndex, columnIndex),
+                        height = treeHeight,
+                        leftNeighbours = leftNeighbours,
+                        rightNeighbours = rightNeighbours,
+                        topNeighbours = topNeighbours,
+                        bottomNeighbours = bottomNeighbours,
+                    )
+                )
+            }
+        }
+        return trees
+    }
+
+    data class Tree(
+        val position: Position,
+        val height: Int,
+        val leftNeighbours: List<Int>,
+        val rightNeighbours: List<Int>,
+        val topNeighbours: List<Int>,
+        val bottomNeighbours: List<Int>,
+    ) {
+        fun isVisible(): Boolean = leftNeighbours.isEmpty()
+                || rightNeighbours.isEmpty()
+                || topNeighbours.isEmpty()
+                || bottomNeighbours.isEmpty()
+                || height > leftNeighbours.maxOf { it }
+                || height > rightNeighbours.maxOf { it }
+                || height > topNeighbours.maxOf { it }
+                || height > bottomNeighbours.maxOf { it }
+
+        fun scenicScore(): Int {
+            val leftScore = if (leftNeighbours.isEmpty()) 0 else leftNeighbours.viewDistance(height)
+            val rightScore = if (rightNeighbours.isEmpty()) 0 else rightNeighbours.viewDistance(height)
+            val topScore = if (topNeighbours.isEmpty()) 0 else topNeighbours.viewDistance(height)
+            val bottomScore = if (bottomNeighbours.isEmpty()) 0 else bottomNeighbours.viewDistance(height)
+            return leftScore * rightScore * topScore * bottomScore
+        }
+
+        fun print(): String {
+            return toString() + " :: visible: ${isVisible()} :: scenic score: ${scenicScore()}"
+        }
+    }
 
 }
+
+fun List<Int>.viewDistance(height: Int): Int {
+    var viewDistance = 0
+    var blocked = false
+    this.map {
+        if (!blocked) {
+            viewDistance++
+            if (it >= height) blocked = true
+        }
+    }
+    return viewDistance
+}
+
+typealias Position = Pair<Int, Int>
--- src/test/kotlin/day08/Day08Test.kt
+++ src/test/kotlin/day08/Day08Test.kt
@@ -8,6 +8,7 @@ internal class Day08Test {
 
     private lateinit var day: Day08
     private val testInput: List<String> = Day08().testInput
+    private val input: List<String> = Day08().input
 
     @BeforeEach
     fun setUp() {
@@ -15,14 +16,25 @@ internal class Day08Test {
     }
 
     @Test
-    fun `part 1 should return 0`() {
-        day.part1(testInput) shouldBe 0
+    fun `part 1 should return 21`() {
+        day.part1(testInput) shouldBe 21
 
     }
 
     @Test
-    fun `part 2 should return 0`() {
-        day.part2(testInput) shouldBe 0
+    fun `part 1 with real data should return 1801`() {
+        day.part1(input) shouldBe 1801
+
+    }
+
+    @Test
+    fun `part 2 should return 8`() {
+        day.part2(testInput) shouldBe 8
+    }
+
+    @Test
+    fun `part 2 with real data should return 209880`() {
+        day.part2(input) shouldBe 209880
     }
 
 }
